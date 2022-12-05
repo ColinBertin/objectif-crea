1932fffdc9836af8b7ad0a8a0a978900514f80c0
Colin Bertin <colinbertin12@gmail.com>
2022-12-05 18:29:50 +0900

    add pading margin to navbar

--- pages/components/navBar/NavBar.tsx
+++ pages/components/navBar/NavBar.tsx
@@ -2,14 +2,14 @@ import Image from 'next/image';
 
 export default function NavBar() {
   return (
-    <nav className="flex justify-between px-12 w-full absolute left-0 top-0 py-5">
+    <nav className="flex justify-between px-20 py-12 w-full absolute left-0 top-0 py-5">
       <div className='m-0 p-0'>
         <Image
           src="/fullLogo.png"
           alt="logo"
           quality={60}
-          width={200}
-          height={150}
+          width={250}
+          height={200}
         />
       </div>
       <ul className="flex my-auto">
