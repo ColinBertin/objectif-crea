3dbf9300bb400aebdcad9e85507d890a1d333dc5
Colin Bertin <colinbertin12@gmail.com>
2022-12-05 20:41:48 +0900

    upper case title

--- pages/components/banner/Banner.tsx
+++ pages/components/banner/Banner.tsx
@@ -6,12 +6,14 @@ export default function Banner() {
         backgroundImage: `url('/bannerImg1.png')`,
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
-        backgroundAttachment: "fixed"
-        // filter: "blur(2px)"
+        backgroundAttachment: "fixed",
       }}
     >
-      <div className="fixed h-screen w-screen -z-1"></div>
-      {/* <h1 className="absolute">Do you see me?</h1> */}
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h3 className="text-xl tracking-widest">— WE TAKE THE TIME TO —</h3>
+        <h1 className="text-4xl tracking-widest">STAND & STARE</h1>
+        <button type="button" className="tracking-widest text-white-500 hover:text-white-700 py-3 px-10 border border-white-500 hover:border-white-700">BE INSPIRED</button>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,15 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {fontSize: {
+      sm: '1rem',
+      base: '1.5rem',
+      xl: '2rem',
+      "2xl": '4rem',
+      "3xl": '6rem',
+      "4xl": '8rem',
+      "5xl": '10rem',
+    }},
   },
   plugins: [],
 }
