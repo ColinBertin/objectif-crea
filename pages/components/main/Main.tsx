3eada324a05663c8c91bd8d036260176fbd667ee
Colin Bertin <colinbertin12@gmail.com>
2022-12-05 18:26:12 +0900

    add font

--- pages/components/main/Main.tsx
+++ pages/components/main/Main.tsx
@@ -2,7 +2,7 @@ export default function Main() {
   return (
     <div className="h-96 text-center pt-28 pb-20 py-9">
       <div className="px-80">
-        <h2>— WE TAKE THE TIME TO —</h2>
+        <h2 className="mb-3">— WE TAKE THE TIME TO —</h2>
         <p>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam
           commodi quis esse, qui voluptatum distinctio perspiciatis. Ut
