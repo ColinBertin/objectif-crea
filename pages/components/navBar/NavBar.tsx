228afc3fe99ae3d131579d966b4d9cb1023c8022
Colin Bertin <colinbertin12@gmail.com>
2022-12-06 14:11:35 +0900

    add icon

--- pages/components/navBar/NavBar.tsx
+++ pages/components/navBar/NavBar.tsx
@@ -1,9 +1,13 @@
-import Image from 'next/image';
+import { useState } from "react";
+import Image from "next/image";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
 export default function NavBar() {
+  const [menuOpen, setMenuOpen] = useState(true);
+
   return (
-    <nav className="flex justify-between px-20 py-12 w-full absolute left-0 top-0 py-5">
-      <div className='m-0 p-0'>
+    <nav className="flex justify-between my-3 px-16 py-12 w-full absolute left-0 top-0 py-5">
+      <div className="m-0 p-0">
         <Image
           src="/fullLogo.png"
           alt="logo"
@@ -12,11 +16,20 @@ export default function NavBar() {
           height={200}
         />
       </div>
-      <ul className="flex my-auto">
-        <li className='mr-2'>Home</li>
-        <li className='mr-2'>Photo</li>
-        <li>Info</li>
-      </ul>
+      <div className="flex">
+        {menuOpen ? (
+          <ul className="flex my-auto">
+            <li className="mr-2">Home</li>
+            <li className="mr-2">Photo</li>
+            <li>Info</li>
+          </ul>
+        ) : (
+          ""
+        )}
+        <button className="">
+          {menuOpen ? <XMarkIcon className="" /> : <Bars3Icon />}
+        </button>
+      </div>
     </nav>
-  )
+  );
 }
