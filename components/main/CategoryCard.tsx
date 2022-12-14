2218057304018adc2dc380c1ee035f650719e8d3
Colin Bertin <colinbertin12@gmail.com>
2022-12-14 14:41:17 +0900

    flex test

--- components/main/CategoryCard.tsx
+++ components/main/CategoryCard.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function CategoryCard() {
   return (
     <div className="max-w-sm shadow-md shadow-gray-600/10">
-      <a className="relative block h-52 bg-white object-fill" href="#">
+      <a className="relative block h-52 w-72 bg-white object-fill" href="#">
         <Image
           className=""
           src="/bannerImg1.png"
@@ -11,7 +11,7 @@ export default function CategoryCard() {
           alt=""
         />
       </a>
-      <div className="flex flex-row h-24 w-96 bg-gray-800">
+      <div className="flex flex-row h-24 bg-gray-800">
         <h4 className="m-auto">LANDSCAPE</h4>
       </div>
     </div>
--- components/main/Main.tsx
+++ components/main/Main.tsx
@@ -2,7 +2,7 @@ import CategoryCard from "./CategoryCard";
 
 export default function Main() {
   return (
-    <div className="text-center py-9">
+    <div className="text-center py-9 px-36">
       <div className="mt-6 px-10 lg:mt-36 lg:px-80 text-white">
         <h3 className="mb-3 text-xl">— WE TAKE THE TIME TO —</h3>
         <p className="text-sm md:text-base leading-9 tracking-wide text-gray-500">
@@ -18,7 +18,7 @@ export default function Main() {
           debitis perspiciatis.
         </p>
       </div>
-      <div className="grid grid-cols-4 grid-rows-2 grid-flow-col gap-4 my-36 mx-12 p-12 place-items-center text-gray-500">
+      <div className="flex flex-wrap justify-center gap-12 py-36 place-items-center text-gray-500">
         <CategoryCard />
         <CategoryCard />
         <CategoryCard />
