2011a80b73af30c58d0627c45d0fe750358177ec
Colin Bertin <colinbertin12@gmail.com>
2022-12-14 14:58:46 +0900

    refactor main comp margin

--- components/main/Main.tsx
+++ components/main/Main.tsx
@@ -2,8 +2,8 @@ import CategoryCard from "./CategoryCard";
 
 export default function Main() {
   return (
-    <div className="text-center py-9 px-20">
-      <div className="text-white">
+    <div className="text-center py-12 px-20">
+      <div className="p-12 text-white">
         <h3 className="mb-3 text-xl">— WE TAKE THE TIME TO —</h3>
         <p className="text-sm md:text-base leading-9 tracking-wide text-gray-500">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam
@@ -18,7 +18,7 @@ export default function Main() {
           debitis perspiciatis.
         </p>
       </div>
-      <div className="flex flex-wrap justify-center gap-12 place-items-center text-gray-500">
+      <div className="flex flex-wrap justify-center gap-12 place-items-center text-gray-500 p-12">
         <CategoryCard />
         <CategoryCard />
         <CategoryCard />
