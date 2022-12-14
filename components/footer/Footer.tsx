efd6d0dafe14f7f136cb86457fff26ea477112ab
Colin Bertin <colinbertin12@gmail.com>
2022-12-14 15:26:05 +0900

    change size logo and icons

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -14,22 +14,22 @@ export default function Footer() {
     <footer className="bg-gray-800 px-14 py-16">
       <div className="mb-10">
       <a className="m-0 p-0" href={"/"} onClick={handleClick}>
-        <Image
+      <Image
           src="/fullLogo.png"
           alt="logo"
           quality={60}
-          width={250}
-          height={200}
+          width={180}
+          height={130}
         />
       </a>
       </div>
       <div className="flex justify-between">
-        <p className="hidden md:inline-block text-gray-500 my-auto">© Copyright 2012 - 2022 | All Rights Reserved</p>
+        <p className="hidden md:inline-block text-gray-500 my-auto text-sm">© Copyright 2012 - 2022 | All Rights Reserved</p>
         <ul className="flex text-gray-600">
-          <li className="mr-4 text-md p-3 rounded bg-gray-700"><FaFacebookF size={40}/></li>
-          <li className="mr-4 text-md p-3 rounded bg-gray-700"><AiOutlineTwitter size={40}/></li>
-          <li className="mr-4 text-md p-3 rounded bg-gray-700"><AiFillInstagram size={40}/></li>
-          <li className="text-md p-3 rounded bg-gray-700"><AiFillYoutube size={40}/></li>
+          <li className="mr-4 text-md p-3 rounded bg-gray-700"><FaFacebookF size={30}/></li>
+          <li className="mr-4 text-md p-3 rounded bg-gray-700"><AiOutlineTwitter size={30}/></li>
+          <li className="mr-4 text-md p-3 rounded bg-gray-700"><AiFillInstagram size={30}/></li>
+          <li className="text-md p-3 rounded bg-gray-700"><AiFillYoutube size={30}/></li>
         </ul>
       </div>
     </footer>
