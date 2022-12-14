4e1c85b1d4906e7ee5ab6f427be5085be24c6717
Colin Bertin <colinbertin12@gmail.com>
2022-12-14 13:34:15 +0900

    fix image

--- components/navBar/NavBar.tsx
+++ components/navBar/NavBar.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 import Image from "next/image";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import clsx from "clsx";
@@ -10,9 +10,9 @@ export default function NavBar() {
   const router = useRouter();
 
   const handleClick = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
-    e.preventDefault()
-    router.push("/")
-  }
+    e.preventDefault();
+    router.push("/");
+  };
 
   return (
     <nav className="flex justify-between px-6 md:px-16 w-full absolute left-0 top-0 py-5 text-white">
@@ -28,15 +28,31 @@ export default function NavBar() {
       <div className="flex">
         {menuOpen ? (
           <ul className={clsx(menuOpen ? "flex my-auto" : "flex my-auto")}>
-            <li className="mr-3"><a href={"/"} onClick={handleClick}>Home</a></li>
-            <li className="mr-3"><a href={"/"} onClick={handleClick}>Photo</a></li>
-            <li className="mr-4"><a href={"/"} onClick={handleClick}>Info</a></li>
+            <li className="mr-3">
+              <a href={"/"} onClick={handleClick}>
+                Home
+              </a>
+            </li>
+            <li className="mr-3">
+              <a href={"/"} onClick={handleClick}>
+                Photo
+              </a>
+            </li>
+            <li className="mr-4">
+              <a href={"/"} onClick={handleClick}>
+                Info
+              </a>
+            </li>
           </ul>
         ) : (
           ""
         )}
         <button className="" onClick={() => setMenuOpen(!menuOpen)}>
-          {menuOpen ? <XMarkIcon className="h-8 w-8 md:h-10 md:w-10" /> : <Bars3Icon className="h-8 w-8 md:h-10 md:w-10" />}
+          {menuOpen ? (
+            <XMarkIcon className="h-8 w-8 md:h-10 md:w-10" />
+          ) : (
+            <Bars3Icon className="h-8 w-8 md:h-10 md:w-10" />
+          )}
         </button>
       </div>
     </nav>
