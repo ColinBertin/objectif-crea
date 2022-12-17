b6f0ba992e028f1fcb9809f75fbc0f490cc15535
Colin Bertin <colinbertin12@gmail.com>
2022-12-18 01:39:03 +0900

    add test page

--- components/navBar/NavBar.tsx
+++ components/navBar/NavBar.tsx
@@ -1,23 +1,16 @@
 import { useState } from "react";
-import { useRouter } from "next/router";
 import Link from 'next/link'
 import Image from "next/image";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import {GiHamburgerMenu} from "react-icons/gi";
+import {AiOutlineClose} from "react-icons/ai";
 import clsx from "clsx";
 
 export default function NavBar() {
   const [menuOpen, setMenuOpen] = useState(false);
 
-  const router = useRouter();
-
-  const handleClick = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
-    e.preventDefault();
-    router.push("/");
-  };
-
   return (
     <nav className="flex justify-between px-4 md:px-12 w-full absolute left-0 top-0 py-10 text-white">
-      <Link className="m-0 p-0" href={"/"} onClick={handleClick}>
+      <Link className="m-0 p-0" href={"/"}>
         <Image
           src="/fullLogo.png"
           alt="logo"
@@ -30,17 +23,17 @@ export default function NavBar() {
         {menuOpen ? (
           <ul className={clsx(menuOpen ? "flex my-auto" : "flex my-auto")}>
             <li className="mr-3">
-              <Link href={"/"} onClick={handleClick}>
+              <Link href={"/"}>
                 Home
               </Link>
             </li>
             <li className="mr-3">
-              <Link href={"/"} onClick={handleClick}>
+              <Link href={"/"}>
                 Photo
               </Link>
             </li>
             <li className="mr-4">
-              <Link href={"/"} onClick={handleClick}>
+              <Link href={"/"}>
                 Info
               </Link>
             </li>
@@ -50,9 +43,9 @@ export default function NavBar() {
         )}
         <button className="" onClick={() => setMenuOpen(!menuOpen)}>
           {menuOpen ? (
-            <XMarkIcon className="h-8 w-8 md:h-10 md:w-10" />
+            <AiOutlineClose className="h-6 w-6 md:h-8 md:w-8" />
           ) : (
-            <Bars3Icon className="h-8 w-8 md:h-10 md:w-10" />
+            <GiHamburgerMenu className="h-6 w-6 md:h-8 md:w-8" />
           )}
         </button>
       </div>
---
+++ pages/testPage/index.tsx
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import NavBar from "../../components/navBar/NavBar";
+import Banner from "../../components/banner/Banner";
+
+export default function testPage() {
+  return (
+    <>
+      <Head>
+        <title>Test</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div>
+        <NavBar />
+        <Banner />
+        <h3>Hello test page</h3>
+      </div>
+    </>
+  );
+}
