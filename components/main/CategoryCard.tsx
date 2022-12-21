750503b66d330a233d05a76fffed9c8eca1f4750
Colin Bertin <colinbertin12@gmail.com>
2022-12-21 18:54:11 +0900

    add blur to card pending

--- components/main/CategoryCard.tsx
+++ components/main/CategoryCard.tsx
@@ -1,25 +1,30 @@
-import Image from 'next/image';
-import Link from 'next/link'
+import Image from "next/image";
+import Link from "next/link";
 
 type categoryCard = {
   title: string;
   link?: string;
   bg?: string;
-}
+};
 
-export default function CategoryCard({title, link, bg}: categoryCard) {
+export default function CategoryCard({ title, link, bg }: categoryCard) {
   return (
     <div className="max-w-sm w-full">
-      <Link className="relative block h-48 bg-white object-fill" href={link ? link : "/"}>
+      <Link
+        className="relative block h-48 bg-white object-fill"
+        href={link ? link : "/"}
+      >
         <Image
-          className="hover:backdrop-blur-sm hover:bg-white/30"
+          className="hover:blur-sm"
           src={bg ? bg : "/images/banner/bannerImg1.png"}
           fill
           alt=""
         />
       </Link>
       <div className="flex flex-row py-5 bg-gray-800">
-        <h5 className="m-auto"><Link href={link ? link : "/"}>{title}</Link></h5>
+        <h5 className="m-auto">
+          <Link href={link ? link : "/"}>{title}</Link>
+        </h5>
       </div>
     </div>
   );
