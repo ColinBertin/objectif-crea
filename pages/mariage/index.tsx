e73e5b6664e3a159fde25487189ed0de5f77dd2d
Colin Bertin <colinbertin12@gmail.com>
2022-12-28 13:06:50 +0900

    add photo album

--- pages/mariage/index.tsx
+++ pages/mariage/index.tsx
@@ -3,17 +3,27 @@ import NavBar from "../../components/navBar/NavBar";
 import Banner from "../../components/banner/Banner";
 import Footer from "../../components/footer/Footer";
 
+import PhotoAlbum from "react-photo-album";
+import {images} from "./img";
+
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+
 export default function Mariage() {
+
   return (
     <>
       <Head>
         <title>Mariage</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <div>
+      <header>
         <NavBar />
         <Banner bg="/images/banner/mariage.png" />
-      </div>
+      </header>
+      <main className="py-20 px-20">
+        <PhotoAlbum layout="columns" photos={images} targetRowHeight={150}/>
+      </main>
       <Footer />
     </>
   );
