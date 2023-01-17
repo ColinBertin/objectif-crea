aee22a46013aed340e6e28e49e403bfd1a327af6
Colin Bertin <colinbertin12@gmail.com>
2023-01-17 18:25:12 +0900

    change language to french

--- components/navBar/Menu.tsx
+++ components/navBar/Menu.tsx
@@ -9,13 +9,13 @@ export default function Menu({menuState}: menuProps) {
   return (
     <ul className={clsx(menuState ? "flex my-auto" : "flex my-auto")}>
       <li className="mr-3">
-        <Link href={"/"}>Home</Link>
+        <Link href={"/"}>Top</Link>
       </li>
       <li className="mr-4">
-        <Link href={"/"}>About</Link>
+        <Link href={"/"}>À propos</Link>
       </li>
       <li className="mr-4">
-        <Link href={"/"}>Contact</Link>
+        <Link href={"/"}>Contacte</Link>
       </li>
     </ul>
   );
