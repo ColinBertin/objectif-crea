28c536c1a0f346c78f84ed235002820f95a22f76
Colin Bertin <colinbertin12@gmail.com>
2023-01-26 22:30:31 +0900

    keep github

--- components/banner/Banner.tsx
+++ components/banner/Banner.tsx
@@ -18,12 +18,12 @@ export default function Banner({ bg }: banner) {
         <h1 className="text-center text-xl sm:text-2xl md:text-4xl tracking-widest mb-10">
           NOTRE OBJECTIF EST DE RÉALISER VOTRE PROJET CRÉATIF !
         </h1>
-        {/* <button
+        <button
           type="button"
           className="md:tracking-widest py-1.5 md:py-3 px-4 md:px-10 border border-white-500 hover:border-white-700"
         >
           <h3 className="text-base sm:text-lg md:text-xl">BE INSPIRED</h3>
-        </button> */}
+        </button>
       </div>
     </div>
   );
