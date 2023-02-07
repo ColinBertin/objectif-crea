5b2afbc4e99f0f6527ce6787ad3b307b2dc4677c
Colin Bertin <colinbertin12@gmail.com>
2023-02-07 14:04:03 +0900

    change heigth card

--- components/main/CategoryCard.tsx
+++ components/main/CategoryCard.tsx
@@ -11,7 +11,7 @@ export default function CategoryCard({ title, link, bg }: categoryCard) {
   return (
     <div className="max-w-sm w-full hover:shadow-[0_4px_50px_5px_rgba(100,100,100,0.2)]">
       <Link
-        className="relative block h-48 bg-white object-fill"
+        className="relative block h-40 bg-white object-fill"
         href={link ? link : "/"}
       >
         <Image
