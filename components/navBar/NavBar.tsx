35372b38481c5939d735ac80b4c5cbaa1207da9a
Colin Bertin <colinbertin12@gmail.com>
2023-04-05 22:25:08 +0900

    fix position card

--- components/navBar/NavBar.tsx
+++ components/navBar/NavBar.tsx
@@ -16,8 +16,8 @@ export default function NavBar() {
 
   return (
     <nav className="w-full fixed top-0 left-0 right-0 z-10 bg-gray-700">
-      <div className="justify-between lg:max-w-7xl md:items-center md:flex mx-auto">
-        <div className="flex justify-between px-4 py-2">
+      <div className="w-full justify-between md:items-center md:flex mx-auto">
+        <div className="flex justify-between px-6 py-2">
           {/* LOGO */}
           <Link href={"/"}>
             <Image
@@ -40,7 +40,7 @@ export default function NavBar() {
             )}
           </button>
         </div>
-        <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center px-6">
            <Menu menuState={navbar} handleNavbar={handleNavbar} />
         </div>
       </div>
