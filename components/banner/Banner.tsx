894e9abc312f30a1e61ceb7607fcffa1c976266e
Colin Bertin <colinbertin12@gmail.com>
2023-04-14 18:23:13 +0900

    add helpers files

--- components/banner/Banner.tsx
+++ components/banner/Banner.tsx
@@ -1,25 +1,12 @@
 import Link from "next/link";
 import { BsChevronCompactDown } from "react-icons/bs";
+import { handleScroll } from "../../helpers";
 
 type banner = {
   bg: string;
 };
 
 export default function Banner({ bg }: banner) {
-
-  const handleScroll = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-    // first prevent the default behavior
-    e.preventDefault();
-    // get the href and remove everything before the hash (#)
-    const href = e.currentTarget.href;
-    const targetId = href.replace(/.*\#/, "");
-    // get the element by id and use scrollIntoView
-    const elem = document.getElementById(targetId);
-    console.log(elem)
-    elem?.scrollIntoView({
-      behavior: "smooth",
-    });
-  };
   return (
     <div
       className="w-screen h-screen"
