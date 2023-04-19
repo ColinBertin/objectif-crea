138b6f08102afcb6bccfc308d420688898036aeb
Colin Bertin <colinbertin12@gmail.com>
2023-04-19 17:01:49 +0900

    correcting miss

--- helpers/index.ts
+++ helpers/index.ts
@@ -10,7 +10,7 @@ export const categories = [
     bg: "/images/banner/maternite.jpg",
   },
   {
-    title: "Nouveau née",
+    title: "Nouveau Née",
     link: "/newBorn",
     bg: "/images/banner/nouveauNee.png",
   },
