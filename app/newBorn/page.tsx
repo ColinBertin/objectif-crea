449d741929d42b1b666c624185ff4fe8a1ca5aa2
Colin Bertin <colinbertin12@gmail.com>
2023-09-12 20:00:48 +0900

    migrate newborn

--- app/newBorn/page.tsx
+++ app/newBorn/page.tsx
@@ -1,17 +1,16 @@
-import Head from "next/head";
 import NavBar from "../../components/navBar/NavBar";
 import Banner from "../../components/banner/Banner";
 import Footer from "../../components/footer/Footer";
 import { categories } from "../../helpers";
 import CategoryCard from "../../components/main/CategoryCard";
 
-export default function NouveauNee() {
+export const metadata = {
+  title: "Nouveau née",
+};
+
+export default function NewBornPage() {
   return (
     <>
-      <Head>
-        <title>Nouveau née</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
       <div>
         <NavBar />
         <Banner bg="/images/banner/nouveauNee.png" />
