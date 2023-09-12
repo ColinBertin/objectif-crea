c3dcbdc815acd2d3ca0ce9573f64db0145d9c9c6
Colin Bertin <colinbertin12@gmail.com>
2023-09-12 21:25:30 +0900

    add prevent scrolling when menu open

--- components/navBar/Menu.tsx
+++ components/navBar/Menu.tsx
@@ -1,8 +1,9 @@
-"use client"
+"use client";
 import clsx from "clsx";
 import Link from "next/link";
 import { handleScroll } from "../../helpers";
 import { Transition } from "@headlessui/react";
+import { useEffect } from "react";
 
 type menuProps = {
   menuState: boolean;
@@ -10,6 +11,15 @@ type menuProps = {
 };
 
 export default function Menu({ menuState, handleNavbar }: menuProps) {
+
+  useEffect(() => {
+    if (menuState) {
+      document.body.classList.add("overflow-y-hidden");
+    } else {
+      document.body.classList.remove("overflow-y-hidden");
+    }
+  }, [menuState]);
+
   return (
     <>
       {/* DESKTOP MENU */}
@@ -18,29 +28,29 @@ export default function Menu({ menuState, handleNavbar }: menuProps) {
           "h-screen w-1/2 md:w-auto md:h-auto md:flex hidden md:block"
         )}
       >
-        <li className="pb-6 md:pb-0 md:py-0 text-xl text-white py-6 md:px-4 text-center border-b-2 md:border-b-0 md:hover:text-gray-300">
+        <li className="pb-6 md:pb-0 md:py-0 text-md text-white py-6 md:px-4 text-center border-b-2 md:border-b-0 md:hover:text-gray-300">
           <Link href={"#banner"} onClick={handleScroll}>
             Top
           </Link>
         </li>
-        <li className="pb-6 md:pb-0 md:py-0 text-white py-6 md:px-4 text-center border-b-2 md:border-b-0 md:hover:text-gray-300">
+        <li className="pb-6 md:pb-0 md:py-0 text-md text-white py-6 md:px-4 text-center border-b-2 md:border-b-0 md:hover:text-gray-300">
           <Link href={"/"}>À propos</Link>
         </li>
-        <li className="pb-6 md:pb-0 md:py-0 text-xl text-white py-6 md:px-4 text-center border-b-2 md:border-b-0 md:hover:text-gray-300">
+        <li className="pb-6 md:pb-0 md:py-0 text-md text-white py-6 md:px-4 text-center border-b-2 md:border-b-0 md:hover:text-gray-300">
           <Link href={"/"}>Contact</Link>
         </li>
       </ul>
-        {/* MOBILE PHONE MENU */}
-        <Transition
-            show={menuState}
-            className="w-full flex justify-center items-center"
-            enter="transition ease-in-out duration-300 transform"
-            enterFrom="-translate-x-full"
-            enterTo="translate-x-0"
-            leave="transition ease-in-out duration-300 transform"
-            leaveFrom="translate-x-0"
-            leaveTo="-translate-x-full"
-          >
+      {/* MOBILE PHONE MENU */}
+      <Transition
+        show={menuState}
+        className="w-full flex justify-center items-center"
+        enter="transition ease-in-out duration-300 transform"
+        enterFrom="-translate-x-full"
+        enterTo="translate-x-0"
+        leave="transition ease-in-out duration-300 transform"
+        leaveFrom="translate-x-0"
+        leaveTo="-translate-x-full"
+      >
         <ul
           className={clsx(
             menuState ? "flex flex-col justify-center -mt-20 mb-20" : "hidden",
@@ -58,7 +68,7 @@ export default function Menu({ menuState, handleNavbar }: menuProps) {
             </Link>
           </li>
         </ul>
-        </Transition>
+      </Transition>
     </>
   );
 }
