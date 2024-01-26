36a5861caee9ed6cb3d6677ce1255203d3f8c660
Colin Bertin <colinbertin12@gmail.com>
2024-01-26 13:56:24 +0900

    add default metadata & template

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,24 @@
 import { Analytics } from "@vercel/analytics/react";
 import "../styles/globals.css";
 
-export const metadata = {
-  title: 'Objectif Créa',
+export async function generateMetadata() {
+  return {
+    title: {
+      default: "Objectif Créa",
+      template: `%s | Objectif Créa`,
+    },
+  };
 }
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>{children}</body>
       <Analytics />
     </html>
-  )
+  );
 }
