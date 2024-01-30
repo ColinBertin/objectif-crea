f76b7c8c4565a93f7e440710d4392a944ce1a0cb
Colin Bertin <colinbertin12@gmail.com>
2024-01-30 11:26:29 +0900

    add transition to categoryCard

--- components/main/CategoryCard.tsx
+++ components/main/CategoryCard.tsx
@@ -10,7 +10,7 @@ type categoryCard = {
 
 export default function CategoryCard({ title, link, bg }: categoryCard) {
   return (
-    <div className="max-w-xs w-full hover:shadow-[0_4px_50px_5px_rgba(100,100,100,0.2)]">
+    <div className="max-w-xs w-full hover:shadow-[0_4px_50px_5px_rgba(100,100,100,0.2)] ease-in duration-200">
       <Link href={link ? link : "/"}>
         <div className="relative block h-40 bg-white object-fill">
           <Image
