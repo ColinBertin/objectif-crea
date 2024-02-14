ad8ad094b7e049ada7aa2fe0ba95b898083211fc
Colin Bertin <colinbertin12@gmail.com>
2024-02-14 17:40:06 +0900

    remove text-sm

--- components/main/Main.tsx
+++ components/main/Main.tsx
@@ -6,13 +6,13 @@ export default function Main() {
     <div id="intro" className="text-center">
       <div className="px-10 py-8 lg:px-40 lg:py-20 text-white">
         <h3 className="mb-3 text-xl">— WE TAKE THE TIME TO —</h3>
-        <p className="text-sm md:text-base leading-9 tracking-wide text-gray-300">
+        <p className="text-base leading-9 tracking-wide text-gray-300">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam
           commodi quis esse, qui voluptatum distinctio perspiciatis. Ut
           quibusdam amet iste sit veniam, minima recusandae incidunt fugiat,
           voluptatem commodi natus debitis!
         </p>
-        <p className="text-sm md:text-base leading-9 tracking-wide text-gray-300">
+        <p className="text-base leading-9 tracking-wide text-gray-300">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
           corrupti maxime sapiente autem accusamus dolore, sunt facilis. Laborum
           et id distinctio. Ullam sunt iusto, quae provident voluptate animi
