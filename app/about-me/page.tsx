e2869c5653f1577b3a78f9741884de7f0859a4bd
Colin Bertin <colinbertin12@gmail.com>
2024-02-14 16:54:00 +0900

    adjsut section of the about-me

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -3,6 +3,8 @@ import Banner from "../../components/banner/Banner";
 import Footer from "../../components/footer/Footer";
 import Image from "next/image";
 
+import ContactForm from "./contact-form";
+
 export const metadata = {
   title: "À Propos",
 };
@@ -29,10 +31,10 @@ export default function AboutMePage() {
             histoires.
           </p>
         </div>
-        <div className="flex justify-between px-10 py-8 lg:px-40 lg:py-20 flex-wrap md:flex-nowrap">
-          <div className="flex flex-col justify-center w-1/2">
+        <div className="flex justify-center items-center space-y-8 px-10 py-8 lg:px-20 xl:px-40 lg:py-20 flex-wrap">
+          <div className="flex flex-col justify-center lg:w-1/2">
             <h4 className="mb-6 text-white">Mon Voyage Photographique</h4>
-            <p className="text-sm md:text-base lg:text-lg leading-9 tracking-wide text-gray-300">
+            <p className="text-sm md:text-base lg:text-lg leading-7 lg:leading-9 md:p-4 tracking-wide text-gray-300">
               Bienvenue sur le portfolio d&apos;Adam Krings, un photographe
               passionné qui a su évoluer avec son art, passant de la chambre
               noire traditionnelle au dynamisme du numérique. Diplômé en arts et
@@ -42,18 +44,20 @@ export default function AboutMePage() {
               histoires.
             </p>
           </div>
-          <Image
-            src="/images/photos/road-min.jpg"
-            alt="nouveau ne"
-            quality={60}
-            width={550}
-            height={550}
-          />
+          <div className="md:w-1/2">
+            <Image
+              src="/images/photos/road-min.jpg"
+              alt="nouveau ne"
+              quality={60}
+              width={550}
+              height={550}
+            />
+          </div>
         </div>
-        <div className="flex flex-row-reverse justify-between px-10 py-8 lg:px-40 lg:py-20 flex-wrap">
-          <div className="flex flex-col justify-center w-1/2">
+        <div className="flex flex-row-reverse justify-center items-center space-y-8 px-10 py-8 lg:px-20 xl:px-40 lg:py-20 flex-wrap">
+          <div className="flex flex-col justify-center md:w-1/2">
             <h4 className="mb-6 text-white">Mon Approche</h4>
-            <p className="text-sm md:text-base lg:text-lg leading-9 tracking-wide text-gray-300">
+            <p className="text-sm md:text-base lg:text-lg leading-7 lg:leading-9 md:p-4 tracking-wide text-gray-300">
               Je crois fermement que chaque photographie doit raconter une
               histoire, capturer un instant, une émotion, un fragment de temps
               qui, autrement, serait perdu. Que ce soit à travers les paysages
@@ -62,13 +66,15 @@ export default function AboutMePage() {
               ordinaires, de souligner la lumière unique de chaque scène.
             </p>
           </div>
-          <Image
-            src="/images/photos/deck-min.jpg"
-            alt="nouveau ne"
-            quality={60}
-            width={550}
-            height={550}
-          />
+          <div className="md:w-1/2">
+            <Image
+              src="/images/photos/deck-min.jpg"
+              alt="nouveau ne"
+              quality={60}
+              width={550}
+              height={550}
+            />
+          </div>
         </div>
         {/* <div className="flex flex-col px-10 py-8 lg:px-40 lg:py-20 text-white">
           <h4 className="mb-6">Mes Services</h4>
@@ -97,7 +103,7 @@ export default function AboutMePage() {
           </p>
         </div> */}
         <section className="bg-gray-500">
-          <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+          <div className="py-8 lg:pb-16 px-4 mx-auto max-w-screen-md">
             <div className="pt-8 md:pt-20 mb-3">
               <h4 className="uppercase text-white">Contactez-Moi</h4>
             </div>
@@ -106,51 +112,7 @@ export default function AboutMePage() {
               pouvons collaborer pour donner vie à votre vision, n&apos;hésitez
               pas à me contacter.
             </p>
-            <form action="mailto:rhythm_53@yahoo.co.jp" className="space-y-8">
-              <div className="space-y-8 text-start">
-                <div>
-                  <label className="block mb-2 text-sm font-medium text-white">
-                    Vorte Email
-                  </label>
-                  <input
-                    type="email"
-                    id="email"
-                    className="shadow-sm bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-300 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                    placeholder="name@example.com"
-                    required
-                  />
-                </div>
-                <div>
-                  <label className="block mb-2 text-sm font-medium text-white">
-                    Suject
-                  </label>
-                  <input
-                    type="text"
-                    id="subject"
-                    className="block p-3 w-full text-sm bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-300 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                    placeholder="Faite-moi savoir comment puis-je vous aider"
-                    required
-                  />
-                </div>
-                <div className="sm:col-span-2">
-                  <label className="block mb-2 text-sm font-medium text-white">
-                    Votre message
-                  </label>
-                  <textarea
-                    id="message"
-                    rows={6}
-                    className="block p-2.5 w-full text-sm bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-300 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                    placeholder="Laissez-moi un message..."
-                  ></textarea>
-                </div>
-              </div>
-              <button
-                type="submit"
-                className="py-3 px-5 text-sm font-medium text-center text-white transition ease-in-out duration-200 transform rounded-lg bg-gray-200 sm:w-fit hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-              >
-                Envoyer
-              </button>
-            </form>
+            <ContactForm />
           </div>
         </section>
       </div>
