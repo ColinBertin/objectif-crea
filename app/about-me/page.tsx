bb02411c260d4260b7d28dd3a3fa42c3d87ed1b0
Colin Bertin <colinbertin12@gmail.com>
2024-02-14 12:55:18 +0900

    fix position

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -106,41 +106,43 @@ export default function AboutMePage() {
               pouvons collaborer pour donner vie à votre vision, n&apos;hésitez
               pas à me contacter.
             </p>
-            <form action="#" className="space-y-8">
-              <div>
-                <label className="block mb-2 text-sm font-medium text-white">
-                  Vorte Email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  className="shadow-sm bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-300 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                  placeholder="name@example.com"
-                  required
-                />
-              </div>
-              <div>
-                <label className="block mb-2 text-sm font-medium text-white">
-                  Suject
-                </label>
-                <input
-                  type="text"
-                  id="subject"
-                  className="block p-3 w-full text-sm bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-300 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                  placeholder="Faite-moi savoir comment puis-je vous aider"
-                  required
-                />
-              </div>
-              <div className="sm:col-span-2">
-                <label className="block mb-2 text-sm font-medium text-white">
-                  Votre message
-                </label>
-                <textarea
-                  id="message"
-                  rows={6}
-                  className="block p-2.5 w-full text-sm bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-300 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                  placeholder="Laissez-moi un message..."
-                ></textarea>
+            <form action="mailto:rhythm_53@yahoo.co.jp" className="space-y-8">
+              <div className="space-y-8 text-start">
+                <div>
+                  <label className="block mb-2 text-sm font-medium text-white">
+                    Vorte Email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    className="shadow-sm bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-300 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                    placeholder="name@example.com"
+                    required
+                  />
+                </div>
+                <div>
+                  <label className="block mb-2 text-sm font-medium text-white">
+                    Suject
+                  </label>
+                  <input
+                    type="text"
+                    id="subject"
+                    className="block p-3 w-full text-sm bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-300 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                    placeholder="Faite-moi savoir comment puis-je vous aider"
+                    required
+                  />
+                </div>
+                <div className="sm:col-span-2">
+                  <label className="block mb-2 text-sm font-medium text-white">
+                    Votre message
+                  </label>
+                  <textarea
+                    id="message"
+                    rows={6}
+                    className="block p-2.5 w-full text-sm bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-300 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                    placeholder="Laissez-moi un message..."
+                  ></textarea>
+                </div>
               </div>
               <button
                 type="submit"
