cee920d5624520a2c8a3d91dfabf4a397d5f0285
Colin Bertin <colinbertin12@gmail.com>
2024-02-16 19:28:10 +0900

    fix btn size

--- components/topButton/TopButton.tsx
+++ components/topButton/TopButton.tsx
@@ -30,7 +30,7 @@ export default function TopButton() {
       id="topBtn"
       className={`${
         !isScrolling && "hidden"
-      } fixed bottom-28 right-7 z-50 bg-gray-300 text-white cursor-pointer p-1 rounded-full transition ease-in-out duration-300 transform`}
+      } fixed bottom-28 right-7 z-50 bg-gray-300 text-white cursor-pointer p-2 text-md md:p-2 md:text-2xl rounded-full transition ease-in-out duration-300 transform`}
       href={"#banner"}
       onClick={handleScroll}
     >
