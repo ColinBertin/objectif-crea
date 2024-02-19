cd1a63847f7d68303bb91f675a22fa907a276db8
Colin Bertin <colinbertin12@gmail.com>
2024-02-19 22:58:06 +0900

    transition to TopButton

--- components/topButton/TopButton.tsx
+++ components/topButton/TopButton.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 import { IoIosArrowRoundUp } from "react-icons/io";
 import { handleScroll } from "../../helpers";
+import { Transition } from "@headlessui/react";
 
 export default function TopButton() {
   const [isScrolling, setIsScrolling] = useState(false);
@@ -26,15 +27,26 @@ export default function TopButton() {
     };
   }, []);
   return (
-    <Link
-      id="topBtn"
-      className={`${
-        !isScrolling && "hidden"
-      } fixed bottom-28 right-7 z-50 bg-gray-300 text-white cursor-pointer p-2 text-md md:p-2 md:text-2xl rounded-full transition ease-in-out duration-300 transform`}
-      href={"#banner"}
-      onClick={handleScroll}
+    <Transition
+      show={isScrolling}
+      className="w-full flex justify-center items-center"
+      enter="transition ease-in duration-150 transform"
+      enterFrom="-translate-x-full"
+      enterTo="translate-x-0"
+      leave="transition ease-out duration-150 transform"
+      leaveFrom="translate-x-0"
+      leaveTo="-translate-x-full"
     >
-      <IoIosArrowRoundUp />
-    </Link>
+      <Link
+        id="topBtn"
+        className={`${
+          !isScrolling && "hidden"
+        } fixed bottom-28 right-7 z-50 bg-gray-300 text-white cursor-pointer p-2 text-md md:p-2 md:text-2xl rounded-full`}
+        href={"#banner"}
+        onClick={handleScroll}
+      >
+        <IoIosArrowRoundUp />
+      </Link>
+    </Transition>
   );
 }
