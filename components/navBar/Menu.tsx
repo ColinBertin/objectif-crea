2cd62e9f9bb1a4555d42d9bd14e1e407ede27163
Colin Bertin <colinbertin12@gmail.com>
2024-03-12 13:55:41 +0900

    fix navbar menu

--- components/navBar/Menu.tsx
+++ components/navBar/Menu.tsx
@@ -58,18 +58,23 @@ export default function Menu({ menuState, handleNavbar }: menuProps) {
         menuState
           ? "h-screen w-screen fixed md:h-auto md:w-64 md:top-10 md:right-5"
           : "hidden",
-        "flex flex-col justify-center px-20 md:px-10 md:pt-5 md:pb-10 bg-gray-800 md:mt-8 z-40 md:shadow-[0_4px_50px_5px_rgba(100,100,100,0.1)]"
+        "flex flex-col justify-center px-20 md:px-8 bg-gray-800 md:mt-8 md:pt-2 md:pb-6 z-40 md:shadow-[0_4px_50px_5px_rgba(100,100,100,0.1)] md:rounded"
       )}
     >
-      <ul className={clsx(menuState && "-mt-16 md:mt-0")}>
+      <ul
+        className={clsx(
+          menuState && "-mt-16 md:mt-0",
+          "text-center md:text-start"
+        )}
+      >
         {sections.map((section) => (
           <li
             key={section.title}
             className={`${
               isSamePath(section.href)
                 ? "text-gold-500 border-gold-500"
-                : "text-white border-white hover:text-gray-200 hover:border-gray-200"
-            } text-xl md:text-base py-5 md:py-3 text-center border-b-2`}
+                : "text-white border-white md:border-gray-200 hover:text-gray-200 hover:border-gray-200"
+            } text-xl md:text-base py-5 md:py-3 border-b-2 md:border-b`}
           >
             <Link href={section.href} onClick={handleNavbar}>
               {section.title}
