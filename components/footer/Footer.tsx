df9f107923e33ca828f58184ae5df0995cd8bed0
Colin Bertin <colinbertin12@gmail.com>
2024-03-14 13:01:35 +0900

    refactor platform link

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,21 +1,34 @@
 "use client";
 
 import Image from "next/image";
-// import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { FaFacebookF } from "react-icons/fa";
-import {
-  // AiOutlineTwitter,
-  AiFillInstagram,
-  AiFillLinkedin,
-} from "react-icons/ai";
+import { AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
+import clsx from "clsx";
 
 export default function Footer() {
-  // const router = useRouter()
-  // const handleClick = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
-  //   e.preventDefault();
-  //   router.push("/");
-  // };
+  const platforms = [
+    {
+      name: "facebook",
+      href: "https://www.facebook.com/profile.php?id=100077455703233",
+      icon: <FaFacebookF size={20} />,
+    },
+    {
+      name: "instagram",
+      href: "https://www.instagram.com/objectif_crea",
+      icon: <AiFillInstagram size={20} />,
+    },
+    {
+      name: "linkedin",
+      href: "https://www.linkedin.com/in/objectif-cr%C3%A9a-756144255/",
+      icon: <AiFillLinkedin size={20} />,
+    },
+    // {
+    //   name: "twitter",
+    //   href: "https://twitter.com/ObjectifCrea",
+    //   icon: <AiOutlineTwitter size={20} />,
+    // },
+  ];
 
   return (
     <footer className="flex justify-between items-center flex-wrap md:block bg-gray-800 px-6 sm:px-12 py-6">
@@ -33,35 +46,18 @@ export default function Footer() {
           © Copyright 2012 - 2022 | All Rights Reserved
         </p>
         <ul className="flex text-gray-600 text-white">
-          <Link
-            href="https://www.facebook.com/profile.php?id=100077455703233"
-            target={"_blank"}
-          >
-            <li className="mr-2 sm:mr-3 md:mr-4 text-md p-2 md:p-3 rounded bg-gray-300 hover:bg-gold-500 transition ease-in-out duration-200 transform">
-              <FaFacebookF size={20} />
-            </li>
-          </Link>
-          {/* <Link href="https://twitter.com/ObjectifCrea" target={"_blank"}>
-            <li className="mr-2 sm:mr-3 md:mr-4 text-md p-2 md:p-3 rounded bg-gray-300 hover:bg-gray-300">
-              <AiOutlineTwitter size={20} />
-            </li>
-          </Link> */}
-          <Link
-            href="https://www.instagram.com/objectif_crea"
-            target={"_blank"}
-          >
-            <li className="mr-2 sm:mr-3 md:mr-4 text-md p-2 md:p-3 rounded bg-gray-300 hover:bg-gold-500 transition ease-in-out duration-200 transform">
-              <AiFillInstagram size={20} />
-            </li>
-          </Link>
-          <Link
-            href="https://www.linkedin.com/in/objectif-cr%C3%A9a-756144255/"
-            target={"_blank"}
-          >
-            <li className="text-md p-2 md:p-3 rounded bg-gray-300 hover:bg-gold-500 transition ease-in-out duration-200 transform">
-              <AiFillLinkedin size={20} />
-            </li>
-          </Link>
+          {platforms.map((platform, i) => (
+            <Link key={platform.name} href={platform.href} target={"_blank"}>
+              <li
+                className={clsx(
+                  i !== platforms.length - 1 && "mr-2 sm:mr-3 md:mr-4",
+                  "text-md p-2 md:p-3 rounded bg-gray-300 hover:bg-gold-500 transition ease-in-out duration-200 transform"
+                )}
+              >
+                {platform.icon}
+              </li>
+            </Link>
+          ))}
         </ul>
       </div>
     </footer>
