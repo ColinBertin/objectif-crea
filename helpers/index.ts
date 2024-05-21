d21681b23856037c11c174bfab47229d2ffdb5b9
Colin Bertin <colinbertin12@gmail.com>
2024-05-21 17:14:17 +0900

    fix scroll issue

--- helpers/index.ts
+++ helpers/index.ts
@@ -23,8 +23,12 @@ export const categories = [
 
 export const handleScroll = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
   e.preventDefault();
-  window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
+  // get the href and remove everything before the hash (#)
+  const href = e.currentTarget.href;
+  const targetId = href.replace(/.*\#/, "");
+  // get the element by id and use scrollIntoView
+  const elem = document.getElementById(targetId);
+  elem?.scrollIntoView({
+    behavior: "smooth",
+  });
 };
